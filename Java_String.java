40386aff9006f764b07bacf75a17b60e14f2bb9b
Brahmleen Kaur <brahmleen.coding@gmail.com>
2023-11-29 01:25:54 +0530

    added Java_String.java file

---
+++ Java_String.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Java_String {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1,s2;
+        System.out.println("Enter first string");
+        s1 = s.nextLine();
+        System.out.println("Enter second string");
+        s2 = s.nextLine();
+        System.out.println("The length of the first string is " + s1.length());
+        System.out.println("The length of the second string is " + s2.length());
+        if(s1.length()==s2.length()){
+            System.out.println("The lengths of the two strings are the same");
+        }
+        else{
+            System.out.println("The lengths of the two strings are not the same");
+        }
+        if(s1.equals(s2)){
+            System.out.println("Two strings are equal");
+        }
+        else{
+            System.out.println("Two strings are not equal");
+        }
+    }
+}
